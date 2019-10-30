53033f742f4d621edcf42bb771483da1b984e034
Marco Cutecchia <marco.cutecchia@studenti.unimi.it>
2019-10-30 17:44:39 +0100

    calculator with functors

--- ocaml/lab1/calculator.ml
+++ ocaml/lab1/calculator.ml
@@ -2,14 +2,62 @@
 open Printf
 
 
-module Calculator : sig
+module type StackADT = sig
+  type 'a stack
+
+  val create: unit -> 'a stack
+  val push: 'a -> 'a stack -> unit
+  val pop: 'a stack -> 'a
+  val length: 'a stack -> int
+end
+
+module SimpleStack : StackADT = struct
+  type 'a stack = {mutable stack: 'a list}
+
+  let create () = {stack=[]}
+  
+  let push x s = 
+    s.stack <- (x::s.stack)
+  
+  let pop s =
+    match s.stack with
+    | [] -> failwith "stack is empty"
+    | x :: xs -> (s.stack <- xs ) ; x
+  
+  let length s = 
+    List.length s.stack
+end
+
+module LimitedStack : StackADT = struct
+  type 'a stack = {mutable stack: 'a list}
+
+  let create () = {stack=[]}
+  
+  let push x s = 
+    if List.length s.stack < 5 then
+      s.stack <- (x::s.stack)
+    else failwith "max 5 elements allowed"
+  
+  let pop s =
+    match s.stack with
+    | [] -> failwith "stack is empty"
+    | x :: xs -> (s.stack <- xs ) ; x
+  
+  let length s = 
+    List.length s.stack
+end
+
+module type CalculatorADT = sig
   type expr
 
   exception InvalidExpression
 
   val string_to_expr: string -> expr
+  val expr_to_string: expr -> string
   val eval: expr -> int
-end = struct
+end
+
+module Calculator (Stack: StackADT) : CalculatorADT = struct
   exception InvalidExpression
 
   type expr =
@@ -33,6 +81,14 @@ end = struct
     List.iter handle_token (Str.split (Str.regexp " +") s) ;
     if Stack.length stack = 1 then Stack.pop stack else raise InvalidExpression
   
+  let rec expr_to_string = function
+    | Val x       -> string_of_int x
+    | Sum (a, b)  -> "(" ^ (expr_to_string a) ^ " + " ^ (expr_to_string b) ^ ")"
+    | Sub (a, b)  -> "(" ^ (expr_to_string a) ^ " - " ^ (expr_to_string b) ^ ")"
+    | Mul (a, b)  -> "(" ^ (expr_to_string a) ^ " * " ^ (expr_to_string b) ^ ")"
+    | Div (a, b)  -> "(" ^ (expr_to_string a) ^ " / " ^ (expr_to_string b) ^ ")"
+    | Pow (a, b)  -> "(" ^ (expr_to_string a) ^ " ** " ^ (expr_to_string b) ^ ")"
+  
   let rec eval = function
     | Val x       -> x
     | Sum (a, b)  -> (eval a) + (eval b)
@@ -41,3 +97,10 @@ end = struct
     | Div (a, b)  -> (eval a) / (eval b)
     | Pow (a, b)  -> int_of_float ((float_of_int (eval a)) ** (float_of_int (eval b))) 
 end
+
+module MyCalc = Calculator (SimpleStack)
+
+let () =
+  let sample = MyCalc.string_to_expr "3 5 + 6 * 12 /"
+  in
+  printf "%s\n" (MyCalc.expr_to_string sample)
